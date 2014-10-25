21f565e1faf5a3ad41e78bbe03fdb0c9babb2b90
pburdi <pburdi@gmail.com>
2014-10-25 07:49:22 -0500

    homework #2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function initializes and makes a cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmax<-function(solve) m<<- solve
+  getmax<-function() m
+  list(set=set, get=get,
+       setmax=setmax,
+       getmax=getmax)
 }
 
-
 ## Write a short comment describing this function
+## This function resolves the cached matrix and takes the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getmax()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmax(m)
+  m
 }
